feb149d0c4bacc721766c69fc4039a30c30f6739
barmelumad11 <100305811+barmelumad11@users.noreply.github.com>
2022-07-04 17:20:45 +0300

    Added a score dto

---
+++ Models/Score.js
@@ -0,0 +1,8 @@
+class Score {
+    constructor (username, movesPlayed, boardSize, date) {
+        this.username = username;
+        this.movesPlayed = movesPlayed;
+        this.boardSize = boardSize;
+        this.date = date;
+    }
+}
